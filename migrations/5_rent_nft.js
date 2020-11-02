1f5162e6e657044c8ed6ce0c35b24f22bd573c0f
nazariyv <nazariy@inbox.ru>
2020-11-02 15:42:28 +0000

    [+] fixes

--- migrations/5_rent_nft.js
+++ migrations/5_rent_nft.js
@@ -1,9 +1,9 @@
-const RentNftResolver = artifacts.require("RentNftResolver");
+const RentNftAddressProvider = artifacts.require("RentNftAddressProvider");
 const RentNft = artifacts.require("RentNft");
 
 module.exports = async (_deployer, _network) => {
   if (_network === "development" || _network === "goerli") {
-    const resolver = await RentNftResolver.deployed();
+    const resolver = await RentNftAddressProvider.deployed();
     await _deployer.deploy(RentNft, resolver.address);
   }
 };
