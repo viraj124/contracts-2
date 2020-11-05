55bdbbba16516969bfc209095140e27ee5f7f781
Naz <nazariy@inbox.ru>
2020-11-05 11:11:35 +0000

    [*] rebase

---
+++ v1/mocks/PaymentToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PaymentToken is ERC20 {
+  constructor() public ERC20("DAI", "DAI") {
+    _mint(address(msg.sender), 10000000000000000000000000);
+  }
+}
