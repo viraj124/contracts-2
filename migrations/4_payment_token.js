89d5623fe43839f196bf287d3f9dfbbb62b4ec08
nazariyv <nazariy@inbox.ru>
2020-11-10 22:41:05 +0000

    [+] added fDAI faucet & revamped the deploy scripts

---
+++ migrations/4_payment_token.js
@@ -0,0 +1,11 @@
+const PaymentToken = artifacts.require("PaymentToken");
+const Faucet = artifacts.require("Faucet");
+
+module.exports = async (_deployer, _network) => {
+  if (_network === "development" || _network === "goerli") {
+    await _deployer.deploy(PaymentToken);
+    const faucet = await Faucet.deployed();
+    const token = await PaymentToken.deployed();
+    await token.transfer(faucet.address, "1000000000000000000000000000");
+  }
+};
